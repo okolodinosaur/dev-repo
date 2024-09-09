4a16d03d16e566de61f7eb2139b56ae473a7dd5a
DenisIvanov00 <densaupin2000@gmail.com>
2024-09-09 11:35:29 +0300

    Added functions 'main' and action functions. Also added basic print of notes list

--- main.py
+++ main.py
@@ -29,74 +29,63 @@ notes : list[dict] = []
 status = {1: "Complete", 2: "Progress", 3: "Backlog", 4: "Waiting"}
 
 
-while True:
-    action = input("add/delete/edit/show: ")
-    if action == "add":
-        print("Type note name (up to 14 symbols)")
-        note_title = input()
-        if len(note_title) > 14:
-            print("Too long name")
-            continue
-        print("Type note content (up to 50 symbols)")
-        note_content = input("content: ")
-        if len(note_content) > 50:
-            print("Too long content")
-            continue
-        print("choose note's status...")
-        for key, value in status.items():
-                print(key, " ", value)
-        try:
-            note_status_input = int(input())
-        except ValueError:
-            continue    
-        note_status = status.get(note_status_input)
-        note = {"title": note_title, "content": note_content, "status": note_status}
-        notes.append(note)
-    elif action == "exit":
-        break
-    elif action == "show":
-        print("############################################################################################")
-        print("# INDEX ###    TITLE    ###                    CONTENT                       ### STATUS    #")
-        print("############################################################################################")
-        for idx, data in enumerate(notes):
-            title = data["title"][:14].ljust(14)
-            content = data["content"][:50].ljust(50)
-            temp_status = data["status"].ljust(8)
-            print(f"    {idx:<2}    | {title} | {content} | {temp_status} |")
-    elif action == "delete":
-        for i,note in enumerate(notes):
-            print(note,i)
-        try:
-            value = int(input("what delete: "))
-        except ValueError:
-            continue
-        if 0 <= value <= len(notes)-1:
-            notes.pop(value)
-        else:
-            print("ne tot index")        
-        print("string")
-    elif action == "edit":
-        for i, note in enumerate(notes):
-            print(note,i)
-        try:
-            value = int(input("what edit: "))
-        except ValueError:
-            continue
-        if 0 <= value <= len(notes)-1:
-            pass
-        else:
-            print("ne tot index")
-            continue
-        choise = input("title/content/status or all: ")
-        if choise == "title":
-            notes[value]["title"] = input("title: ")
-        if choise == "content":
-            notes[value]["content"] = input("content: ")
-        if choise == "content":
-            notes[value]["status"] = input("status: ")
-        if choise == "all":
-            notes[value]["title"] = input("title: ")
-            notes[value]["content"] = input("content: ")
-            notes[value]["status"] = input("status: ")
+def add_note(notes, status):
+    print("Type note name (up to 14 symbols)")
+    note_title = input()
+    if len(note_title) > 14:
+        print("Too long name")
+        return
+    print("Type note content (up to 50 symbols)")
+    note_content = input("content: ")
+    if len(note_content) > 50:
+        print("Too long content")
+        return
+    print("choose note's status...")
+    for key, value in status.items():
+            print(key, " ", value)
+    try:
+        note_status_input = int(input())
+    except ValueError:
+        return    
+    note_status = status.get(note_status_input)
+    note = {"title": note_title, "content": note_content, "status": note_status}
+    notes.append(note)        
+
+
+def edit_note(notes):
+    for i, note in enumerate(notes):
+        print(note,i)
+    try:
+        value = int(input("what edit: "))
+    except ValueError:
+        return
+    if 0 <= value <= len(notes)-1:
+        pass
     else:
-        print("Wrong command...\nTry again")
+        print("ne tot index")
+        return
+    choise = input("title/content/status or all: ")
+    if choise == "title":
+        notes[value]["title"] = input("title: ")
+    if choise == "content":
+        notes[value]["content"] = input("content: ")
+    if choise == "content":
+        notes[value]["status"] = input("status: ")
+    if choise == "all":
+        notes[value]["title"] = input("title: ")
+        notes[value]["content"] = input("content: ")
+        notes[value]["status"] = input("status: ")
+        
+
+def main():
+    while True:
+        action = input("Type add/edit/print\n")
+        if action == "add":
+            add_note(notes, status)
+        elif action == "edit":
+            edit_note(notes)
+        elif action == "print":
+             print(notes)
+
+
+main()
